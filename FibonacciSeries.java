3f71b18477467fdb4db4f48670661228b66e7c1f
kalyanbhargav1432 <tannirukalyan1432@gmail.com>
2021-09-29 20:07:34 +0530

    Added: Functionality to check the Fibonacci Series

---
+++ FibonacciSeries.java
@@ -0,0 +1,23 @@
+package Assignment_4;
+
+import java.util.Scanner;
+
+public class FibonacciSeries {
+	public static void main(String[] args) {
+		int FirstNumber = 0;
+		int SecondNumber = 1;
+		int Result;
+		int count = 0;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter any number:");
+		int UserNumber = scanner.nextInt();
+		while (count < UserNumber) {
+			System.out.print("Fibonacci Number:" + count + " - > ");
+			Result = FirstNumber + SecondNumber;
+			FirstNumber = SecondNumber;
+			SecondNumber = Result;
+			count++;
+			System.out.println(FirstNumber + " ");
+		}
+	}
+}
